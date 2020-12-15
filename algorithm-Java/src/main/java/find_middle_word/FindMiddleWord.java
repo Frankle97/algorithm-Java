c38cd8ee2417745cdd4f7a0b41638e7dd228fb9f
Frankle <pairpapa@gmail.com>
2020-12-15 23:30:55 +0900

    Solve the problem

--- algorithm-Java/src/main/java/find_middle_word/FindMiddleWord.java
+++ algorithm-Java/src/main/java/find_middle_word/FindMiddleWord.java
@@ -2,7 +2,9 @@ package find_middle_word;
 
 public class FindMiddleWord {
 	public String solution(String s) {
-    String answer = "";
+    // 테스트
+	String answer = "";
+    
 	int length = s.length();
 	if (length % 2==0) {
 		answer += s.charAt((length/2)-1);
