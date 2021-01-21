fd279545b1670375fded6afdce4303677f2d93d5
Frankle <pairpapa@gmail.com>
2021-01-21 20:23:52 +0900

    Solve the problem

---
+++ algorithm-Java/src/main/java/factorial/Factorial.java
@@ -0,0 +1,27 @@
+package factorial;
+
+import java.util.Scanner;
+
+/*
+문제
+0보다 크거나 같은 정수 N이 주어진다. 이때, N!을 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 정수 N(0 ≤ N ≤ 12)가 주어진다.
+
+출력
+첫째 줄에 N!을 출력한다.
+ */
+public class Factorial {
+	public static int factorial(int n) {
+		if (n <= 1) return 1;
+		return n * factorial(n-1);
+	}
+	
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	
+	System.out.println(factorial(sc.nextInt()));
+	sc.close();
+	}
+}
